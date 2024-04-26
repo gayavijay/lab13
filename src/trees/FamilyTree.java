721357eb8bd3492f2a796630bb9f96cf94e2d7aa
Gayathri Vijay <gayathritharavijay@gmail.com>
2024-04-26 09:39:17 -0700

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -33,6 +33,9 @@ public class FamilyTree
         {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+        	children.add(childNode);
+            childNode.parent = this;
+        	
         }
         
         
@@ -41,7 +44,7 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (this.name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
@@ -49,6 +52,10 @@ public class FamilyTree
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
+            	TreeNode result = child.getNodeWithName(targetName);
+                
+            	if (result != null)
+                    return result;
             }
             
             // Not found anywhere.
@@ -61,11 +68,21 @@ public class FamilyTree
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
+            TreeNode current = this;
+            
+            while (current.parent != null) {
+                ancestors.add(current.parent);
+                current = current.parent;
+            }
+
 
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
+            
+            Collections.reverse(ancestors);
+            
 
             return ancestors;
         }
